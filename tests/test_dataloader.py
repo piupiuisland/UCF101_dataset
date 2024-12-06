875a6719d85f3919c01051a7d25b8d5b41efe600
Ubuntu <ubuntu@ip-10-20-1-126.eu-central-1.compute.internal>
2024-12-06 15:45:14 +0000

    [Add dataset sampler] Add sampler

--- tests/test_dataloader.py
+++ tests/test_dataloader.py
@@ -1,2 +1,3 @@
 from typing import List, Tuple, Dict, Optional
 import pytest
+from ..ucf_dataset import UCFDataSet
---
+++ tests/test_dataset.py
@@ -0,0 +1,74 @@
+from typing import List, Tuple, Dict, Optional
+import pytest
+import numpy as np
+from torch.utils.data import DataLoader
+
+from ..ucf_dataset import UCFDataSet, SubsetSampler
+
+
+LOCAL_UCF_PATH = "/home/ubuntu/work_root/llm_repo/text2video_eval/ucf101_dataset/"
+
+
+@pytest.mark.unittest
+@pytest.mark.parametrize("split", ["train", "test"])
+@pytest.mark.parametrize("video_size", [(720, 480), (320, 240)])
+def test_ucf_dataset(split, video_size):
+
+    dataset = UCFDataSet(path=LOCAL_UCF_PATH,split=split, size=video_size)
+
+    sample_0 = dataset[0]
+
+    sample_1 = dataset[1]
+
+    assert isinstance(sample_0[1], np.ndarray)
+    assert sample_0[1].shape[-2:] == video_size
+    assert sample_1[1].shape[-2:] == video_size
+
+
+
+@pytest.mark.unittest
+@pytest.mark.parametrize("split", ["train", "test"])
+@pytest.mark.parametrize("video_size", [(720, 480)])
+@pytest.mark.parametrize("batch_size", [1, 4])
+def test_ucf_dataloader(split, video_size, batch_size):
+
+    ucf_dataset = UCFDataSet(path=LOCAL_UCF_PATH,split=split, size=video_size)
+
+    dataloader = DataLoader(
+        ucf_dataset, batch_size=batch_size,
+        pin_memory=False, num_workers=4,
+        shuffle=True)
+    sample_0 = next(iter(dataloader))
+
+    sample_0_prompt, sample_0_video = sample_0[0], sample_0[1]
+
+    assert sample_0[1].shape[-2:] == video_size
+    assert len(sample_0_prompt) == batch_size
+    print("prompts: ", sample_0_prompt)
+    print("videos: ", sample_0_video.shape)
+
+
+
+@pytest.mark.unittest
+@pytest.mark.parametrize("split", ["train", "test"])
+@pytest.mark.parametrize("batch_size", [1, 4])
+def test_ucf_dataloader_smapler(split, video_size, batch_size):
+
+    ucf_dataset = UCFDataSet(path=LOCAL_UCF_PATH,split=split, size=(720, 480))
+    sampler = SubsetSampler(ucf_dataset, subset_size=100)
+
+    dataloader = DataLoader(
+        ucf_dataset, batch_size=batch_size,
+        pin_memory=False, num_workers=4,
+        shuffle=False, sampler=sampler)
+    sample_0 = next(iter(dataloader))
+
+    sample_0_prompt, sample_0_video = sample_0[0], sample_0[1]
+
+    assert sample_0[1].shape[-2:] == video_size
+    assert len(sample_0_prompt) == batch_size
+    print("prompts: ", sample_0_prompt)
+    print("videos: ", sample_0_video.shape)
+
+    print("len(full_dataset)", len(ucf_dataset))
+    print("len(subset_dataloader)", len(dataloader))
